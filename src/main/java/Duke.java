fac92aa29aecc6f4d180493c58acc5fdb3471ec3
teachyourselfcoding <e0310446@u.nus.edu>
2020-08-31 17:24:42 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,42 +1,50 @@
 import java.util.Scanner;
 
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        request();
         //Generates a greeting
-        //greeting();
-
-
+        greeting();
+        request();
     }
 
     public static void request(){
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         String line;
-
-        int i=0,j=0;
+        int i=0,j;
 
         while(true) {
+            String status ="[✗] ";
             line = in.nextLine();
+
             if (line.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }else if(line.equals("list")){
                 for(j=0;j<i;j++){
-                    System.out.println((j+1)+". "+list[j]);
+                    if(list[j].isDone){
+                        System.out.println((j+1)+ ". " + "[✓]" + list[j].description);
+                    }//not done
+                    System.out.println((j+1)+ ". " + "[✗] " + list[j].description);
                 }
-            }else {
-                list[i] = line;
+            }else if(line.contains("done")){
+                int num = Integer.parseInt(line.substring(5));
+                System.out.println(num);
+                list[num-1].isDone = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[✓] "+ list[num-1].description);
+            } else {
+                list[i] = new Task(line);
                 System.out.println("added: "+ line);
                 i++;
             }
         }
+    }
+
+    public static void greeting(){
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
     }
 
 }
