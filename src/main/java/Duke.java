2ad3d0441815591c53be4ef36391936444ec66e5
teachyourselfcoding <e0310446@u.nus.edu>
2020-08-31 11:49:55 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
+    }
+
+    public static void greeting(){
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
